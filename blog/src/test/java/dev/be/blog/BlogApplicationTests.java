1ca88130dba607352202be73f7a9be40704b76ac
yoonjuho <dbswn990@naver.com>
2023-05-30 11:49:24 +0900

    Init: 초기 프로젝트 생성

---
+++ blog/src/test/java/dev/be/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
