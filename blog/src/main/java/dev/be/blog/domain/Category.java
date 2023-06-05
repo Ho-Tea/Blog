747ca07975a4b0463b07245d8d67d7b1d41e7745
yoonjuho <dbswn990@naver.com>
2023-06-05 18:50:58 +0900

    Refactor: Category Interface 메서드 수정

--- blog/src/main/java/dev/be/blog/domain/Category.java
+++ blog/src/main/java/dev/be/blog/domain/Category.java
@@ -1,9 +1,12 @@
 package dev.be.blog.domain;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface Category {
-    void printChild();
     void add(Category category);
     void remove(Category category);
-    void printCategory();
+    void rename(String name);
+    String getName();
 
 }
