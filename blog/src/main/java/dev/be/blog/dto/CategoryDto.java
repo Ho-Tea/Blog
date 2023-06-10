aadf8146c30f3f720c530f546ed0bd95ac2b2375
yoonjuho <dbswn990@naver.com>
2023-06-10 19:56:15 +0900

    Feat: Category의 데이터를 표현하는 Dto 객체생성

--- blog/src/main/java/dev/be/blog/dto/CategoryDto.java
+++ blog/src/main/java/dev/be/blog/dto/CategoryDto.java
@@ -1,13 +1,43 @@
 package dev.be.blog.dto;
 
+import dev.be.blog.domain.Category;
+import dev.be.blog.domain.Content;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CategoryDto {
     private final String name;
+    private List<Content> contents = new ArrayList<>();
 
     public CategoryDto(String categoryName) {
         this.name = categoryName;
     }
 
+    private CategoryDto(String categoryName, List<Content> contents){
+        this.name = categoryName;
+        this.contents = List.copyOf(contents);  //방어적 복사
+    }
+
     public String getName() {
         return this.name;
+    }
+
+
+    public static CategoryDto from(Category category){
+        return new CategoryDto(category.getName(), category.getChild());
+    }
+
+    public static Category toEntity(CategoryDto categoryDto){
+        return Category.create(categoryDto.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDto{" +
+                "name='" + name + '\'' +
+                ", contents=" + contents +
+                '}';
     }
 }
