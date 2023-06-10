eee0625eb5b0882e18f6bfb1d2a22947ad66aa9e
yoonjuho <dbswn990@naver.com>
2023-06-10 19:52:20 +0900

    Feat: Blog의 상태를 나타내는 객체는 하나 존재한다

---
+++ blog/src/main/java/dev/be/blog/domain/Blog.java
@@ -0,0 +1,60 @@
+package dev.be.blog.domain;
+
+import dev.be.blog.exception.NotFoundException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Blog {
+    WAIT(0),
+    MAIN(1),
+    WRITE(2),
+    UPDATE(3),
+    DELETE(4),
+    LOOKUP(5),
+    EXIT(6);
+
+    public static Blog status = Blog.WAIT;
+
+    private int statusNumber;
+
+    Blog(int statusNumber) {
+        this.statusNumber = statusNumber;
+    }
+
+
+    public static boolean of() {
+        return status.equals(Blog.MAIN);
+    }
+
+    public static boolean ofWrite() {
+        return status.equals(Blog.WRITE);
+    }
+
+    public static boolean ofUpdate() {
+        return status.equals(Blog.UPDATE);
+    }
+
+    public static boolean ofDelete() {
+        return status.equals(Blog.DELETE);
+    }
+
+    public static boolean ofLookUp() {
+        return status.equals(Blog.LOOKUP);
+    }
+
+    public static boolean ofClose() {
+        return status.equals(Blog.EXIT);
+    }
+
+    public static void transfer(int statusNumber) {
+        status = Arrays.stream(Blog.values()).filter(o -> o.statusNumber == statusNumber)
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException());
+    }
+
+    public static List<Blog> command(){
+        return Arrays.stream(Blog.values()).filter(o -> o.ordinal() > 0).toList();
+    }
+
+}
