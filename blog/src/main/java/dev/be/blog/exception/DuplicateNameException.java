8f5a2311f98f6f3fd402c99c7ecdc30cad09fa0c
yoonjuho <dbswn990@naver.com>
2023-06-10 19:56:39 +0900

    Refactor: Error메시지 변경

--- blog/src/main/java/dev/be/blog/exception/DuplicateNameException.java
+++ blog/src/main/java/dev/be/blog/exception/DuplicateNameException.java
@@ -1,12 +1,11 @@
 package dev.be.blog.exception;
 
 public class DuplicateNameException extends RuntimeException{
-    public static final String MESSAGE_DUPLICATE_NAME = "카테고리에 중복된 이름이 존재합니다.";
+    public static final String MESSAGE_DUPLICATE_NAME = "카테고리 내부에 중복된 이름이 존재합니다.";
 
     public DuplicateNameException() {
         this(MESSAGE_DUPLICATE_NAME);
     }
-
     public DuplicateNameException(String message) {
         super(message);
     }
