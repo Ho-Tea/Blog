5e26ea78a4103e9ec41d48fb5554143164bbd90c
yoonjuho <dbswn990@naver.com>
2023-06-10 19:54:01 +0900

    Refactor: Post를 표현하는 Dto객체

--- blog/src/main/java/dev/be/blog/dto/PostDto.java
+++ blog/src/main/java/dev/be/blog/dto/PostDto.java
@@ -3,30 +3,49 @@ package dev.be.blog.dto;
 import dev.be.blog.domain.Post;
 import dev.be.blog.domain.User;
 
+import java.util.Optional;
+
 public class PostDto {
 
     private final String title;
     private final String text;
+    private final String updatedDate;
+    private final String createdDate;
+    private final String user;
 
-    public PostDto(String title, String text) {
+    public PostDto(String title, String text){
         this.title = title;
         this.text = text;
+        this.updatedDate = Optional.empty().toString();
+        this.createdDate = Optional.empty().toString();
+        this.user = Optional.empty().toString();
     }
 
-    public String getTitle() {
-        return title;
+
+    public PostDto(String title, String text, String updatedDate, String createdDate, String user) {
+        this.title = title;
+        this.text = text;
+        this.updatedDate = updatedDate;
+        this.createdDate = createdDate;
+        this.user = user;
     }
 
-
-    public String getText() {
-        return text;
-    }
-
-    public Post upload(User user){
+    public static Post toEntity(PostDto postDto, User user){
         return Post.builder()
-                .title(title)
-                .text(text)
+                .title(postDto.title)
+                .text(postDto.text)
                 .user(user)
                 .build();
+    }
+
+    @Override
+    public String toString() {
+        return "PostDto{" +
+                "title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                ", updatedDate='" + updatedDate + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", user='" + user + '\'' +
+                '}';
     }
 }
