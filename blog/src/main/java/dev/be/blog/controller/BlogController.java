3558ef375fa32eab59eda0eef53a1b36b0d239aa
yoonjuho <dbswn990@naver.com>
2023-07-26 01:12:20 +0900

    Refactor : Controller를 가볍게 구성

--- blog/src/main/java/dev/be/blog/controller/BlogController.java
+++ blog/src/main/java/dev/be/blog/controller/BlogController.java
@@ -1,16 +1,19 @@
 package dev.be.blog.controller;
 
-import dev.be.blog.constant.Blog;
+import dev.be.blog.constant.BlogCommand;
 import dev.be.blog.constant.ContentType;
-import dev.be.blog.domain.*;
-import dev.be.blog.dto.*;
+import dev.be.blog.domain.Category;
+import dev.be.blog.domain.Post;
+import dev.be.blog.domain.User;
+import dev.be.blog.dto.CategoryDto;
+import dev.be.blog.dto.PostDto;
+import dev.be.blog.dto.UserDto;
 import dev.be.blog.exception.DuplicateNameException;
-import dev.be.blog.exception.IllegalContentTypeException;
 import dev.be.blog.view.InputView;
 import dev.be.blog.view.OutputView;
-import dev.be.blog.dto.ContentDto;
 import dev.be.blog.vo.Rename;
 
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class BlogController {
@@ -18,149 +21,119 @@ public class BlogController {
     private static final InputView INPUT = new InputView();
     private static final OutputView OUTPUT = new OutputView();
     private User user;
-    private Category contents;
-    private Blog blog;
+    private Category rootContent;
+    private BlogCommand blogCommand;
+    private Map<BlogCommand, Supplier> commandResolveMap;
 
-    public BlogController(){
+    public BlogController() {
         UserDto userDto = INPUT.enrollUser();
-        createBlogByUserName(userDto);
-    }
-
-    protected void createBlogByUserName(UserDto userDto) {
         user = UserDto.toEntity(userDto);
-        contents = Category.create(user.getNickname());
+        rootContent = Category.create(user.getNickname());
+        saveCommandList();
+    }
+    //-> 테스트가 주가 되어서는 안된다
+    // 테스트용 생성자를 하나 더 만드는 것도 가능하지만 굳이 그렇게해서 테스트를 진행하지는 않겠다
+
+
+    private void saveCommandList() {
+        commandResolveMap = Map.of(
+                BlogCommand.DEFAULT, this::show,
+                BlogCommand.WRITE, this::write,
+                BlogCommand.UPDATE, this::rename,
+                BlogCommand.DELETE, this::delete,
+                BlogCommand.READ, this::read
+        );
     }
 
     public void run() {
-        while (!blog.isExit()) {
+        while (!blogCommand.isExit()) {
             inputCommand();
             loadCommand();
         }
     }
 
 
-    public void inputCommand() {
-        OUTPUT.commandType();
-        blog = blog.match(INPUT.command());
+    private void inputCommand() {
+        OUTPUT.commandType(); // 가능한 모든 블로그 관련 행동 출력
+        blogCommand = blogCommand.match(INPUT.command()); //블로그 관련 행동 입력 받아 지정
     }
-
 
 
     private void loadCommand() {
-        judgeWrite();
-        judgeUpdate();
-        judgeDelete();
-        judgeRead();
-        judgeDefault();
+        Supplier commandResolve = commandResolveMap.get(blogCommand);
+        repeatLogic(commandResolve);
     }
 
-    private void judgeWrite(){
-        if (blog.isWrite()){
-            repeatLogic(this::write);
-        }
-    }
-    private void judgeUpdate(){
-        if (blog.isUpdate()){
-            repeatLogic(this::rename);
-        }
-    }
-    private void judgeDelete(){
-        if (blog.isDelete()){
-            repeatLogic(this::delete);
-        }
-    }
-    private void judgeRead(){
-        if (blog.isRead()){
-            read();
-        }
-    }
-    private void judgeDefault(){
-        if (blog.isDefault()){
-            show();
-        }
-    }
-
-    private ContentDto createContent() {
-        ContentType contentType = INPUT.selectContentType();
-        CategoryDto parentCategoryDto = INPUT.selectCategory();
-        return new ContentDto(contentType, parentCategoryDto);
-    }
 
     private boolean write() {
-        ContentDto contentDto = createContent();
-        ContentType contentType = contentDto.getContentType();
-        if (contentType.isPost()) {
+        ContentType contentType = INPUT.selectContentType();
+        String parentCategoryName = INPUT.selectParentCategory();
+        if (contentType.is(ContentType.POST)) {
             PostDto postDto = inputPostDetail();
-            return save(postDto, contentDto.getCategoryDto());
-        } else if (contentType.isCategory()){
-            CategoryDto childCategoryDto = inputCategoryDetail();
-            return save(childCategoryDto, contentDto.getCategoryDto());
+            return save(postDto, parentCategoryName);
         }
-        throw new IllegalContentTypeException();
+        CategoryDto categoryDto = inputCategoryDetail();
+        return save(categoryDto, parentCategoryName);
     }
 
 
-
-    private PostDto inputPostDetail(){
+    private PostDto inputPostDetail() {
         PostDto postDto = INPUT.post();
         validateName(postDto.getName());
         return postDto;
     }
-    protected void validateName(String name){
-        if(contents.isExist(name)){
-            throw new DuplicateNameException();
-        }
-    }
 
-    private CategoryDto inputCategoryDetail(){
+    private CategoryDto inputCategoryDetail() {
         CategoryDto categoryDto = INPUT.category();
         validateName(categoryDto.getName());
         return categoryDto;
     }
-    protected  <T> boolean save(T dto, CategoryDto parentCategoryDto) {
-        String categoryName = parentCategoryDto.getName();
-        if (dto.getClass().equals(PostDto.class)) {
-            Post content = PostDto.toEntity((PostDto) dto, user);
-            return contents.findAndAdd(content, categoryName);
+
+    protected void validateName(String name) {
+        if (rootContent.isExist(name)) {
+            throw new DuplicateNameException();
         }
-        Category content = CategoryDto.toEntity((CategoryDto)dto);
-        return contents.findAndAdd(content, categoryName);
     }
 
 
-    private void show() {
-        CategoryDto categoryDto = CategoryDto.fromEntity(contents);
+    protected boolean save(PostDto dto, String parentCategoryName) {
+        Post content = PostDto.toEntity(dto, user);
+        return rootContent.add(content, parentCategoryName);
+    }
+
+    protected boolean save(CategoryDto dto, String parentCategoryName) {
+        Category content = CategoryDto.toEntity(dto);
+        return rootContent.add(content, parentCategoryName);
+    }
+
+
+    private boolean show() {
+        CategoryDto categoryDto = CategoryDto.fromEntity(rootContent);
         OUTPUT.content(categoryDto);
+        return true;
     }
 
-    private void read() {
-        String title = repeatLogic(this::isPostTitle);
-        Post found = (Post) contents.find(title);
+    private boolean read() {
+        String title = INPUT.findPost();
+        Post found = (Post) rootContent.find(title);
         PostDto foundDto = PostDto.fromEntity(found);
         OUTPUT.post(foundDto);
+        return true;
     }
-
 
 
     private boolean rename() {
         Rename rename = INPUT.rename();
-        return contents.findAndRename(rename);
+        return rootContent.rename(rename);
     }
 
     private boolean delete() {
         String name = INPUT.delete();
-        return contents.findAndRemove(name);
+        return rootContent.remove(name);
     }
 
-    private String isPostTitle(){
-        String title = INPUT.findPost();
-        if (contents.find(title).getClass().equals(Category.class)) {
-            throw new IllegalContentTypeException();
-        }
-        return title;
-    }
 
-    private  <T> T repeatLogic(Supplier<T> inputSupplier) {
+    private <T> T repeatLogic(Supplier<T> inputSupplier) {
         try {
             return inputSupplier.get();
         } catch (RuntimeException e) {
