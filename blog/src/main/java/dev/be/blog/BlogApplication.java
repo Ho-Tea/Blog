150c3feae65d79e30e9dcf8d112abd02766fbaaa
yoonjuho <dbswn990@naver.com>
2023-09-11 17:55:10 +0900

    chore : spring 디펜던시 세팅

--- blog/src/main/java/dev/be/blog/BlogApplication.java
+++ blog/src/main/java/dev/be/blog/BlogApplication.java
@@ -1,16 +1,18 @@
 package dev.be.blog;
 
 import dev.be.blog.controller.BlogController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-//        SpringApplication.run(BlogApplication.class, args);
-        BlogController blog = new BlogController();
-        blog.run();
+        SpringApplication.run(BlogApplication.class, args);
+//        BlogController blog = new BlogController();
+//        blog.run();
 
     }
 }
--- blog/src/test/java/dev/be/blog/BlogApplicationTests.java
+++ blog/src/test/java/dev/be/blog/BlogApplicationTests.java
@@ -4,10 +4,10 @@ import dev.be.blog.domain.Category;
 import dev.be.blog.domain.Content;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
-//@SpringBootTest
+@SpringBootTest
 class BlogApplicationTests {
 
     @Test
