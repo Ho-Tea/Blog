3fc356ade95a27669a2934c2487962c78ece5d3e
yoonjuho <dbswn990@naver.com>
2023-09-13 02:53:53 +0900

    WebHookTest(PullRequest)

--- src/test/java/dev/be/blog/BlogApplicationTests.java
+++ src/test/java/dev/be/blog/BlogApplicationTests.java
@@ -1,11 +1,6 @@
 package dev.be.blog;
 
-import dev.be.blog.domain.Category;
-import dev.be.blog.domain.Content;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import java.util.Optional;
 
 @SpringBootTest
 class BlogApplicationTests {
