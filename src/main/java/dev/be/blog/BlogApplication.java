8e341369109cfceff362b067089efe1025f34292
yoonjuho <dbswn990@naver.com>
2023-10-13 21:03:35 +0900

    style: import 최적화 (#5)

--- src/main/java/dev/be/blog/BlogApplication.java
+++ src/main/java/dev/be/blog/BlogApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.io.IOException;
-
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
 
     }
