7da2295452de55aa352712b07b88e84ae978c07c
yoonjuho <dbswn990@naver.com>
2023-10-13 13:52:35 +0900

    feat: Token DTO (#5)

---
+++ src/main/java/dev/be/blog/global/common/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package dev.be.blog.global.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String token;
+
+}
